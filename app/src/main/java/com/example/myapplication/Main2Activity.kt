b0c7c7919a5f75f0fe96917665b797287718044b
931697478@qq.com <931697478@qq.com>
2019-05-27 09:35:10 +0800

    增加字体库选择

--- app/src/main/java/com/example/myapplication/Main2Activity.kt
+++ app/src/main/java/com/example/myapplication/Main2Activity.kt
@@ -1,12 +1,17 @@
 package com.example.myapplication
 
+import android.graphics.Typeface
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main2.*
 
 class Main2Activity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+        val typeface = Typeface.createFromAsset(assets, "impact_font.ttf")
+        tv.typeface = typeface
+        tv.text = "177865465465"
     }
 }
